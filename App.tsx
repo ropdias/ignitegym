1e68aca7be2f6454143aa40d9f03c0082a9acc35
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-10-31 00:43:32 -0300

    feat: refactor App component to use Routes for navigation and update AuthRoutes to hide header in navigation stack

--- App.tsx
+++ App.tsx
@@ -7,7 +7,7 @@ import {
 import { GluestackUIProvider } from '@gluestack-ui/themed'
 import { config } from './config/gluestack-ui.config'
 import { Loading } from '@components/Loading'
-import { SignUp } from '@screens/SignUp'
+import { Routes } from '@routes/index'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular })
@@ -19,7 +19,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <SignUp /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </GluestackUIProvider>
   )
 }
