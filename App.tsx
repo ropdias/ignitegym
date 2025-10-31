5c0651e652c1b933125beb0abe0ef01cfb79a18e
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-10-30 22:34:00 -0300

    feat: replace SignIn screen with SignUp screen in App component and add SignUp component for user registration

--- App.tsx
+++ App.tsx
@@ -7,7 +7,7 @@ import {
 import { GluestackUIProvider } from '@gluestack-ui/themed'
 import { config } from './config/gluestack-ui.config'
 import { Loading } from '@components/Loading'
-import { SignIn } from '@screens/SignIn'
+import { SignUp } from '@screens/SignUp'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular })
@@ -19,7 +19,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      {fontsLoaded ? <SignUp /> : <Loading />}
     </GluestackUIProvider>
   )
 }
