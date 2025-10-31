85ba64777a35d844087bb401b62ea085ff611afa
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-10-31 02:47:47 -0300

    feat: wrap App component with SafeAreaProvider for improved layout handling on different devices

--- App.tsx
+++ App.tsx
@@ -8,18 +8,21 @@ import { GluestackUIProvider } from '@gluestack-ui/themed'
 import { config } from './config/gluestack-ui.config'
 import { Loading } from '@components/Loading'
 import { Routes } from '@routes/index'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular })
 
   return (
-    <GluestackUIProvider config={config}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      {fontsLoaded ? <Routes /> : <Loading />}
-    </GluestackUIProvider>
+    <SafeAreaProvider>
+      <GluestackUIProvider config={config}>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        {fontsLoaded ? <Routes /> : <Loading />}
+      </GluestackUIProvider>
+    </SafeAreaProvider>
   )
 }
