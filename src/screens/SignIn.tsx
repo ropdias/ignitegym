36dda000b0595f6c05c8203249188789130b7537
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-10-30 21:58:38 -0300

    feat: create Input component and integrate it into SignIn screen for email and password fields

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,7 +1,8 @@
-import { Center, Image, Text, VStack } from '@gluestack-ui/themed'
+import { Center, Heading, Image, Text, VStack } from '@gluestack-ui/themed'
 
 import BackgroundImg from '@assets/background.png'
 import Logo from '@assets/logo.svg'
+import { Input } from '@components/Input'
 
 export function SignIn() {
   return (
@@ -14,13 +15,23 @@ export function SignIn() {
         alt="Pessoas treinando"
         position="absolute"
       />
-      <Center my="$24">
-        <Logo />
 
-        <Text color="$gray100" fontSize="$sm">
-          Treine sua mente e seu corpo
-        </Text>
-      </Center>
+      <VStack flex={1} px="$10" pb="$16">
+        <Center my="$24">
+          <Logo />
+
+          <Text color="$gray100" fontSize="$sm">
+            Treine sua mente e seu corpo
+          </Text>
+        </Center>
+
+        <Center gap="$2">
+          <Heading color="$gray100">Acesse sua conta</Heading>
+
+          <Input placeholder="E-mail" />
+          <Input placeholder="Senha" />
+        </Center>
+      </VStack>
     </VStack>
   )
 }
