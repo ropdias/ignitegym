65b2ef410c53ca85f20b215be35114490534e8f2
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-10-31 00:40:14 -0300

    chore: remove .npmrc file and update metro.config.js to support additional file extensions and resolve react-dom for mobile platforms

--- metro.config.js
+++ metro.config.js
@@ -9,10 +9,18 @@ module.exports = (() => {
     ...transformer,
     babelTransformerPath: require.resolve('react-native-svg-transformer/expo'),
   }
+
   config.resolver = {
     ...resolver,
     assetExts: resolver.assetExts.filter((ext) => ext !== 'svg'),
-    sourceExts: [...resolver.sourceExts, 'svg'],
+    sourceExts: [...resolver.sourceExts, 'svg', 'mjs'],
+    resolveRequest: (context, moduleName, platform) => {
+      // Redireciona react-dom para react-native no mobile
+      if (moduleName === 'react-dom' && platform !== 'web') {
+        return context.resolveRequest(context, 'react-native', platform)
+      }
+      return context.resolveRequest(context, moduleName, platform)
+    },
   }
 
   return config
