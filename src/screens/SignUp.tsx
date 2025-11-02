19794f0f528e8e554b9f03654aac90062042118e
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-11-02 12:02:14 -0300

    feat: add email validation and error message display in SignUp form

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -91,6 +91,13 @@ export function SignUp() {
             <Controller
               control={control}
               name="email"
+              rules={{
+                required: 'Informe o email.',
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  message: 'E-mail inválido',
+                },
+              }}
               render={({ field: { onChange, value } }) => (
                 <Input
                   placeholder="E-mail"
@@ -101,6 +108,10 @@ export function SignUp() {
                 />
               )}
             />
+            {errors.email && (
+              <Text color="$white">{errors.email?.message}</Text>
+            )}
+
             <Controller
               control={control}
               name="password"
