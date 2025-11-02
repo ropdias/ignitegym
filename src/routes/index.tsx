a507f99b3043e1cd1795705ced6d7988d944ff78
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-11-02 19:58:11 -0300

    feat: add loading state to AuthContext and conditionally render loading component in Routes for improved user experience

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,12 +5,17 @@ import { Box } from '@gluestack-ui/themed'
 import { AuthRoutes } from './auth.routes'
 import { useAuth } from '@hooks/useAuth'
 import { AppRoutes } from './app.routes'
+import { Loading } from '@components/Loading'
 
 export function Routes() {
   const theme = DefaultTheme
   theme.colors.background = gluestackUIConfig.tokens.colors.gray700
 
-  const { user } = useAuth()
+  const { user, isLoadingUserStorageData } = useAuth()
+
+  if (isLoadingUserStorageData) {
+    return <Loading />
+  }
 
   return (
     <Box flex={1} bg="$gray700">
