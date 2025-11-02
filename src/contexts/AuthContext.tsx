dbbb68f8fc8a8c6419844401b1954d91ceab3f45
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-11-02 19:39:27 -0300

    feat: implement user storage functionality by saving user data to AsyncStorage upon authentication

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,4 +1,5 @@
 import { createContext, ReactNode, useState } from 'react'
+import { storageUserSave } from '@storage/storageUser'
 import { api } from '@services/api'
 
 import { UserDTO } from '@dtos/UserDTO'
@@ -25,6 +26,7 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
 
       if (data.user) {
         setUser(data.user)
+        storageUserSave(data.user)
       }
     } catch (error) {
       throw error
