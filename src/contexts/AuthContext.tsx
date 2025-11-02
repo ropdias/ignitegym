21a8c07555889cfda49fae0f8888057eefc9dd78
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-11-02 20:48:57 -0300

    feat: enhance AuthContext to save user data and authentication token together for improved session management

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -4,6 +4,7 @@ import {
   storageUserSave,
   storageUserRemove,
 } from '@storage/storageUser'
+import { storageAuthTokenSave } from '@storage/storageAuthToken'
 import { api } from '@services/api'
 
 import { UserDTO } from '@dtos/UserDTO'
@@ -27,13 +28,28 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
   const [user, setUser] = useState<UserDTO>({} as UserDTO)
   const [isLoadingUserStorageData, setIsLoadingUserStorageData] = useState(true)
 
+  async function storageUserAndToken(userData: UserDTO, token: string) {
+    try {
+      setIsLoadingUserStorageData(true)
+
+      api.defaults.headers.common['Authorization'] = `Bearer ${token}`
+
+      await storageUserSave(userData)
+      await storageAuthTokenSave(token)
+      setUser(userData)
+    } catch (error) {
+      throw error
+    } finally {
+      setIsLoadingUserStorageData(false)
+    }
+  }
+
   async function signIn(email: string, password: string) {
     try {
       const { data } = await api.post('/sessions', { email, password })
 
-      if (data.user) {
-        setUser(data.user)
-        storageUserSave(data.user)
+      if (data.user && data.token) {
+        storageUserAndToken(data.user, data.token)
       }
     } catch (error) {
       throw error
